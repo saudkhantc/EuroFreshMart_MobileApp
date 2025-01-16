8087130de5fecc5848622b097355c972da4acbd6
AfaqCoding <afaqcoder@gmail.com>
2025-01-15 17:04:34 -0800

    latest code push

--- android/app/src/main/java/com/eurofreshmart_mobileapp/MainActivity.kt
+++ android/app/src/main/java/com/eurofreshmart_mobileapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.eurofreshmart_mobileapp
+package com.ApnaDookan
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "EuroFreshMart_MobileApp"
+  override fun getMainComponentName(): String = "ApnaDookan"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
